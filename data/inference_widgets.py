76f21db3d3d90461d8bba244be7310acdce5e200
David Friederich <dav.friede@gmail.com>
2020-12-04 16:04:17 +0100

    bump inference widget

--- data/inference_widgets.py
+++ data/inference_widgets.py
@@ -7,6 +7,8 @@ import ipywidgets as widgets
 from ipywidgets import Layout, Tab, Button, Label, Box, VBox, HBox, HTML, Output, Dropdown, BoundedIntText, Textarea, IntSlider, jslink, SelectMultiple
 from IPython.display import display
 
+dropdown_layout = Layout(width="150px")
+
 
 def highlight_columns(column):
     if not np.isnan(column.Actual):
@@ -27,22 +29,45 @@ class CroInferenceViewer():
         self.df = df
         self.df_filtered = df
         self.label_list = label_list
-        self.has_true_label = False  # selected_row['has_true_label']
 
         # Initialize buttons
         self.int_input = BoundedIntText(
-            min=0, max=len(self.df), step=1, description="Index:")
+            min=1, max=len(self.df), step=1, description="Index:")
         self.int_slider = IntSlider(
-            readout=False, min=0, max=len(self.df))
+            readout=False, min=1, max=len(self.df))
         self.index_link = jslink(
             (self.int_input, 'value'), (self.int_slider, 'value'))
 
-        # Initialize filter buttons
-        self.cro_cat_filter = SelectMultiple(
-            options=label_list + ["-"],
-            description="Categories",
+        # Initialize labels filter
+        self.cro_cat_actual_filter = SelectMultiple(
+            layout=dropdown_layout,
+            options=self.label_list + ["-"],
+            description="Actual Label",
             # value=label_list
         )
+
+        self.cro_cat_predicted_filter = SelectMultiple(
+            layout=dropdown_layout,
+            options=self.label_list + ["-"],
+            description="Predicted Label",
+            # value=label_list
+        )
+
+        self.adjunct_filter = SelectMultiple(
+            layout=dropdown_layout,
+            options=[True, False],
+            description="Adjunct pages",
+            # value=label_list
+        )
+
+        # Initialize report filters
+        dataset_filters = self.df.labelling_dataset.dropna().unique()
+        self.dataset_filter = SelectMultiple(
+            layout=dropdown_layout,
+            options=dataset_filters.tolist() + ["-"],
+            description="Dataset",
+        )
+
         industries = sorted(self.df.icb_industry.unique())
         self.industries_filter = SelectMultiple(
             options=industries,
@@ -51,12 +76,14 @@ class CroInferenceViewer():
         )
         countries = sorted(self.df.country.unique())
         self.country_filter = SelectMultiple(
+            layout=dropdown_layout,
             options=countries,
             description="Countries",
             # value=countries
         )
         years = sorted(self.df.year.unique())
         self.year_filter = SelectMultiple(
+            layout=dropdown_layout,
             options=years,
             description="Years",
             # value=countries
@@ -64,13 +91,18 @@ class CroInferenceViewer():
 
         # Set observers
         self.int_input.observe(self.on_idx_change, 'value')
-        self.cro_cat_filter.observe(self.handle_filter_change, 'value')
+        self.cro_cat_actual_filter.observe(self.handle_filter_change, 'value')
+        self.cro_cat_predicted_filter.observe(
+            self.handle_filter_change, 'value')
+        self.adjunct_filter.observe(self.handle_filter_change, 'value')
+        self.dataset_filter.observe(self.handle_filter_change, 'value')
         self.industries_filter.observe(self.handle_filter_change, 'value')
         self.country_filter.observe(self.handle_filter_change, 'value')
         self.year_filter.observe(self.handle_filter_change, 'value')
 
         # Render
         self.filter_output = Output()
+        self.summary_output = Output()
         self.paragraph_output = Output()
         self.render()
 
@@ -79,13 +111,40 @@ class CroInferenceViewer():
 
     def handle_filter_change(self, event):
         active_filters = []
-        if len(self.cro_cat_filter.value) > 0:
-            if self.has_true_label:
-                active_filters = [
-                    f"({c} == 1 | {c}_actual) == 1)" for c in self.cro_cat_filter.value]
+        if len(self.cro_cat_actual_filter.value) > 0:
+            only_negative = True if "-" in self.cro_cat_actual_filter.value else False
+            if only_negative:
+                active_filters.append(
+                    "(" + " & ".join([f' {c}_actual == 0 ' for c in self.label_list]
+                                     ) + ")")
             else:
-                active_filters = [
-                    f"{c} == 1" for c in self.cro_cat_filter.value]
+                active_filters.append("(" + " | ".join(
+                    [f' {c}_actual == 1 ' for c in self.cro_cat_actual_filter.value if c != "-"]
+                ) + ")")
+
+        if len(self.cro_cat_predicted_filter.value) > 0:
+            only_negative = True if "-" in self.cro_cat_predicted_filter.value else False
+            if only_negative:
+                active_filters.append(
+                    "(" + " & ".join([f' {c}_predicted == 0 ' for c in self.label_list]
+                                     ) + ")")
+            else:
+                active_filters.append("(" + " | ".join(
+                    [f' {c}_predicted == 1 ' for c in self.cro_cat_predicted_filter.value if c != "-"]
+                ) + ")")
+
+        if len(self.dataset_filter.value) > 0:
+            include_nan = True if "-" in self.dataset_filter.value else False
+            if include_nan:
+                active_filters.append(
+                    "(" +
+                    f"labelling_dataset == {[v for v in self.dataset_filter.value if v != '-']}" +
+                    f" or labelling_dataset != labelling_dataset"
+                        + ")"
+                )
+            else:
+                active_filters.append(
+                    f"labelling_dataset == {self.dataset_filter.value}")
 
         if len(self.industries_filter.value) > 0:
             active_filters.append(
@@ -97,10 +156,11 @@ class CroInferenceViewer():
         if len(self.year_filter.value) > 0:
             active_filters.append(f"year == {self.year_filter.value}")
 
+        print(active_filters)
         self.df_filtered = self.df.query(" & ".join(active_filters))
 
         # Reset idx value
-        self.int_input.value = 0
+        self.int_input.value = 1
         self.int_input.max = len(self.df_filtered)
         self.int_slider.max = len(self.df_filtered)
 
@@ -111,16 +171,30 @@ class CroInferenceViewer():
         with self.filter_output:
             display(VBox([
                 HBox([
-                    self.cro_cat_filter,
+                    self.cro_cat_actual_filter,
+                    self.cro_cat_predicted_filter,
+                    self.adjunct_filter,
+                ]),
+                HBox([
+                    self.dataset_filter,
                     self.industries_filter,
                     self.country_filter,
                     self.year_filter
                 ]),
-            ]))
+            ], layout=Layout(border="outset", padding="10px")))
+
+    def render_summary(self):
+        self.summary_output.clear_output()
+
+        with self.summary_output:
+            display(
+                self.df_filtered[[
+                    l + "_predicted" for l in self.label_list]].describe()
+            )
 
     def render_paragraph(self):
         try:
-            selected_row = self.df_filtered.iloc[self.int_input.value]
+            selected_row = self.df_filtered.iloc[self.int_input.value - 1]
         except IndexError:
             self.paragraph_output.clear_output()
             with self.paragraph_output:
@@ -130,10 +204,9 @@ class CroInferenceViewer():
                 return
 
         df_result = pd.DataFrame([
-            selected_row[self.label_list].to_numpy(),
+            selected_row[[l + "_predicted" for l in self.label_list]].to_numpy(),
             selected_row[[l + "_prob" for l in self.label_list]].to_numpy(),
-            selected_row[[l + "_actual" for l in self.label_list]].to_numpy(
-            ) if self.has_true_label else np.empty(len(self.label_list)) * np.nan
+            selected_row[[l + "_actual" for l in self.label_list]].to_numpy()
         ],
             columns=self.label_list, index=[
                 "Predictions", "Probability", "Actual"]
@@ -156,7 +229,9 @@ class CroInferenceViewer():
 
     def render(self):
         self.render_filters()
+        # self.render_summary()
         self.render_paragraph()
 
         display(self.filter_output)
+        display(self.summary_output)
         display(self.paragraph_output)
